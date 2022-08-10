69f7cc5ca549a245cca22f36b6c5b8cead8bad90
Davis Toler <tolerdavis0@gmail.com>
2022-08-10 15:10:54 -0500

    commit tree ef680582704b7e6bb562a3b567cb03cbb1505783

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2 
 
+
+let random = Math.floor(Math.random() * 1000 ) + 1
+
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4
+
+const max = Math.max(5, 10, 15, 20) 
 // Write your code here
